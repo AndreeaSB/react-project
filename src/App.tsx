d0bd21ed0725dcbd6cb5f15630d119e4f94ef9ff
AndreeaSB <andreea.sbadea@gmail.com>
2025-01-15 20:21:56 +0200

    done, kinda

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import RandomColor from './components/random-color-generator'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      {/*Random Color component */}
+      <RandomColor/>
+    </div>
   )
 }
 
